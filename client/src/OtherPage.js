270b379c878b75b1a9af2530ba25be8caf4c7ff8
supciola <olka.kobylska@gmail.com>
2023-01-29 17:43:16 +0100

    commit1

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      AKMultifib
+      Aleksandra Kobylska
+      IMST 2.1/2
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
